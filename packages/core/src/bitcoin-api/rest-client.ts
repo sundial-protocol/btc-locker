954e49e25f7b498b8803992b83fdf33f2920145c
samdelaney <sam@thedelaneys.net>
2026-04-22 15:33:59 -0700

    use configured provider in fee query

--- packages/core/src/bitcoin-api/rest-client.ts
+++ packages/core/src/bitcoin-api/rest-client.ts
@@ -20,7 +20,7 @@ import type {
 
 export class BitcoinRestClient {
   private network: NetworkType;
-  private apiProvider: ApiProvider;
+  readonly apiProvider: ApiProvider;
   private baseUrls: ApiUrls;
   private baseUrl: string;
   private isFallback: boolean;
